3676c96f8920864540c26411cad33cf0c3b61146
filras <filip45.rask@gmail.com>
2025-02-17 16:31:19 +0100

    diesel fix

--- src/api.rs
+++ src/api.rs
@@ -1,31 +1,34 @@
+use self::models::*;
 #[allow(unused)]
 use axum::{
-    http::StatusCode, 
-    Router, 
-    routing::get, 
-    response::{IntoResponse, Response},  
-    extract::{Query, Json}};
+    extract::{Json, Query},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use diesel::prelude::*;
+use serde::Deserialize;
 use serde::Serialize;
+use serde_json::to_writer_pretty;
 use std::fs::File;
 use std::path::Path;
-use serde::{de, Deserialize, Deserializer};
-use serde_json::{to_writer_pretty};
-use std::{fmt, str::FromStr};
+use D0018E_Webshop::*;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Item {
-id: u32,
-title: String,
-description : Option<String>,
-price: String,
-in_stock: u32,
-average_rating: Option<f32>
+    id: u32,
+    title: String,
+    description: Option<String>,
+    price: String,
+    in_stock: u32,
+    average_rating: Option<f32>,
 }
 
 fn default_page() -> usize {
     1
 }
-fn default_per_page () -> usize {
+fn default_per_page() -> usize {
     10
 }
 #[derive(Debug, Deserialize)]
@@ -33,24 +36,20 @@ pub struct Pagination {
     #[serde(default = "default_page")]
     page: usize,
     #[serde(default = "default_per_page")]
-    per_page: usize
-
+    per_page: usize,
 }
 
 async fn write_json(to_write: Item) {
     let file_path = Path::new("./MockData.json");
     let file = File::open(file_path).expect("file not found");
     to_writer_pretty(file, &to_write);
-
 }
 
-async fn read_json () -> Vec<Item>{
-
+async fn read_json() -> Vec<Item> {
     let file_path = Path::new("./MockData.json");
     let file = File::open(file_path).expect("file not found");
-    let items:Vec<Item> = serde_json::from_reader(file).expect("error while parsing");
-    return items
-
+    let items: Vec<Item> = serde_json::from_reader(file).expect("error while parsing");
+    return items;
 }
 // routes for api
 //async fn main(){
@@ -58,33 +57,34 @@ async fn read_json () -> Vec<Item>{
 //let api = Router::new()
 //   .route("/items", get(get_items).post(post_items));
 
-
 //}
 
-async fn paginate_vec(v: Vec<Item>, page: usize, per_page: usize) -> Vec<Item>{
-   let start = (page-1) * per_page;
-   let mut end = start + per_page;
-   if end > v.len(){
+async fn paginate_vec(v: Vec<Item>, page: usize, per_page: usize) -> Vec<Item> {
+    let start = (page - 1) * per_page;
+    let mut end = start + per_page;
+    if end > v.len() {
         end = v.len();
-   }
-   let res = v[start..end].to_vec();
-   
+    }
+    let res = v[start..end].to_vec();
 
     return res;
-
 }
 
-pub async fn get_items(pagination: Query<Pagination>) -> impl IntoResponse { 
+pub async fn get_items(pagination: Query<Pagination>) -> impl IntoResponse {
     let pagination: Pagination = pagination.0;
-    let items:Vec<Item> = read_json().await;
-    let result:Vec<Item> = paginate_vec(items, pagination.page, pagination.per_page).await;
-    (StatusCode::OK, Json(result))        
+    use self::schema::items::dsl::*;
+    let connection = &mut connect_to_db();
+    //   let results = items
+    //  .limit(5)
+    //  .select(Item::as_select())
+    //        .load(connection)
+    //       .expect("Error loading posts");
+    //  (StatusCode::OK, Json(results))
 }
 
 pub async fn post_items(data: Json<Item>) -> impl IntoResponse {
     let rcv_item: Item = data.0;
-    println! ("Recieved Item {:?}", rcv_item);
+    println!("Recieved Item {:?}", rcv_item);
     write_json(rcv_item).await;
     (StatusCode::OK, "Item recieved")
-    
 }
--- src/models.rs
+++ src/models.rs
@@ -1,20 +1,22 @@
 use diesel::prelude::*;
+use diesel::query_dsl::methods::SelectDsl;
+use diesel::select;
 
 #[derive(Queryable, Selectable)]
-#[diesel(table_name = create::schema::items, Selectable)]
+#[diesel(table_name = crate::schema::items)]
 #[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct Item {
     pub id: i32,
     pub title: String,
-    pub description: String,
+    pub description: Option<String>,
     pub price: i32,
     pub in_stock: i32,
-    pub average_rating: f32,
-    pub discounted_price: i32,
+    pub average_rating: Option<f32>,
+    pub discounted_price: Option<i32>,
 }
 
 #[derive(Queryable, Selectable)]
-#[diesel(table_name = create::schema::items, Selectable)]
+#[diesel(table_name = crate::schema::users)]
 #[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct User {
     pub id: i32,
@@ -24,8 +26,8 @@ pub struct User {
     pub surname: String,
     pub email: String,
     pub role: String,
-    pub address: String,
-    pub zipcode: String,
-    pub co: String,
-    pub country: String,
+    pub address: Option<String>,
+    pub zipcode: Option<String>,
+    pub co: Option<String>,
+    pub country: Option<String>,
 }
