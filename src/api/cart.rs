3b4950c7862ad87fb2843eab49ad66eb2ca96466
filras <filip45.rask@gmail.com>
2025-03-04 23:13:59 +0100

    cart GET now returns one combined object

--- src/api/cart.rs
+++ src/api/cart.rs
@@ -1,8 +1,8 @@
 use crate::{
-    auth::{self, ctx::Ctx, session},
+    auth::{self, ctx::Ctx},
     db::{
         connect_to_db,
-        models::{CartItems, Item, User},
+        models::{CartItems, User},
     },
     schema::{
         cart_items::{self, amount, item_id, user_id},
@@ -18,13 +18,25 @@ use axum::{
     routing::get,
     Router,
 };
-use diesel::{prelude::*, query_dsl::methods::FilterDsl};
-use serde::Deserialize;
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 
 pub fn routes() -> Router {
     Router::new()
         .route("/cart", get(get_cart).put(put_cart))
         .layer(middleware::from_fn(auth::middleware::require_auth))
+}
+
+#[derive(Serialize, Queryable)]
+struct CombinedCartItem {
+    item_id: i32,
+    title: String,
+    description: Option<String>,
+    price: i32,
+    in_stock: i32,
+    average_rating: Option<f32>,
+    discounted_price: Option<i32>,
+    amount: i32,
 }
 
 async fn get_cart(ctx: Result<Ctx, String>) -> impl IntoResponse {
@@ -38,10 +50,20 @@ async fn get_cart(ctx: Result<Ctx, String>) -> impl IntoResponse {
     // .first::<User>(conn);
 
     let result = CartItems::belonging_to(&user_obj)
-        .inner_join(items::table)
-        .select((CartItems::as_select(), Item::as_select()))
-        .load::<(CartItems, Item)>(conn)
+        .inner_join(items::table.on(items::id.eq(cart_items::item_id)))
+        .select((
+            items::id,
+            items::title,
+            items::description,
+            items::price,
+            items::in_stock,
+            items::average_rating,
+            items::discounted_price,
+            cart_items::amount,
+        ))
+        .load::<CombinedCartItem>(conn)
         .unwrap();
+
     return (StatusCode::OK, Json(result).into_response());
 }
 
