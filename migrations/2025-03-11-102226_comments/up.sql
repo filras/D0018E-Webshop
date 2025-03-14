5b577559b628766fb94ddfd727e187f31a039a5a
Adam Gatmon <48681209+add33m@users.noreply.github.com>
2025-03-15 00:16:35 +0100

    Fixed cascade delete for comments

--- migrations/2025-03-11-102226_comments/up.sql
+++ migrations/2025-03-11-102226_comments/up.sql
@@ -2,6 +2,6 @@ CREATE TABLE comments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(id),
     review_id INT NOT NULL REFERENCES reviews(id),
-    comment_id INT REFERENCES comments(id),
+    comment_id INT REFERENCES comments(id) ON DELETE CASCADE,
     comment VARCHAR(255) NOT NULL
 );
